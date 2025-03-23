28f1120908566f422ac353f15d48edd2fb3eec8b
Mateus <utracraft900@gmail.com>
2025-03-23 23:47:09 +0000

    A média das despesas

--- conn.py
+++ conn.py
@@ -14,9 +14,30 @@ else:
 cursor = conn.cursor()
 
 cursor.execute("SELECT valor FROM despesas")
-for line in cursor.fetchall():
-    print(line)
+value= cursor.fetchall()
 
+all_vall = []
+
+# Filtrar e converter os valores válidos.
+for i in value:
+    if i[0] is not None:
+        try:
+            vall_num = float(i[0])# Tenta converter diretamente para float
+            all_vall.append(i[0]) # Valor valido adicionar a lista.
+
+        except ValueError:
+            print(f"Valor inválido{i[0]}") # Se não for possível converter, ignora.
+            
+
+# Verificando valores.
+if value:
+    
+    # Calculando media.
+    average = sum(all_vall) / len(all_vall)
+
+    print(f"A média das despesas é: R${round(average,2)}")
+
+else: print("Nenhum valor encontrado ou não e numerico.")
 
 cursor.close()
 conn.close()
